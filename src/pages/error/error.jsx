b9037d7b5aa9c62505c2cf9cc14cf7452c334a08
NC <nicolas.candeli@cegetel.net>
2023-05-14 21:16:15 +0200

    feat: add CSS to error page

--- src/pages/error/error.jsx
+++ src/pages/error/error.jsx
@@ -1,16 +1,21 @@
 import { useRouteError } from 'react-router-dom'
+import Header from '../../layouts/header/header'
+import Sidebar from '../../layouts/sidebar/sidebar'
 
 export default function Error() {
   const error = useRouteError()
   console.error(error)
 
   return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Désolé, une erreur inattendue est survenue.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
+    <>
+      <Header />
+      <div className="page-wrapper">
+        <Sidebar />
+        <div className="error-wrapper">
+          <h1>{error.status}</h1>
+          <p>Désolé, une erreur inattendue est survenue.</p>
+        </div>{' '}
+      </div>
+    </>
   )
 }
